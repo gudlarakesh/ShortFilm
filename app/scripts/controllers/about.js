cd32df7459866fdc21b298c2eeb93d79fa03e9b5
gudlarakesh <gudlarakesh1991@gmail.com>
2016-03-29 14:42:19 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shortFilmApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shortFilmApp
+ */
+angular.module('shortFilmApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shortFilmApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shortFilmApp
+ */
+angular.module('shortFilmApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
